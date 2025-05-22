8d0dc0becc4cdee139c14889731ddafdf50c6486
Shritesh D Jagtap <143780330+ShriteshJagtap-BugendaiTech@users.noreply.github.com>
2025-05-22 15:19:35 +0530

    Update chatbot_main.py

--- chatbot_main.py
+++ chatbot_main.py
@@ -31,7 +31,7 @@ if the user asks a question that is not related to resumes, kindly decline reque
         llm = ChatOpenAI(
             model="llama3-70b-8192",
             base_url="https://api.groq.com/openai/v1",
-            
+            api_key=st.secrets["GROQ_API_KEY"],
             temperature=0.2,
         )
         return llm.invoke(prompt).content
