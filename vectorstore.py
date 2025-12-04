739cb50cdc5f96d2950902656f457c8cf37207cb
Shritesh D Jagtap <143780330+ShriteshJagtap-BugendaiTech@users.noreply.github.com>
2025-12-04 14:45:01 +0530

    Update vectorstore.py

--- vectorstore.py
+++ vectorstore.py
@@ -3,7 +3,8 @@
 import os
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.schema import Document
+from langchain_core.documents import Document
+
 
 VECTORSTORE_DIR = "resume_index"
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
